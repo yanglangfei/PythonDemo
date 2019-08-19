0c3102724b6d44838a87c62ca7bac60696077fcf
yanglf <899117>
2019-08-19 18:34:00 +0800

    词云

--- com/yanglf/main/demo5.py
+++ com/yanglf/main/demo5.py
@@ -8,14 +8,16 @@ import numpy as np
 
 path = os.path
 print(path)
+# 文本清洗  剔除掉切分的特殊字符
 stopwords = {'这些': 0, '那些': 0, '因为': 0, '所以': 0}  # 噪声词
 path_txt = '../file/all.txt'
 # 只有纯白色背景图片才可以渲染成图片的形状
-path_img = '../img/img3.jpg'
+path_img = '../img/img4.png'
 f = open(path_txt, 'r', encoding='UTF-8').read()
 background_image = np.array(Image.open(path_img))
 
 # 结巴分词分割文本，并拼接成字符串
+# jieba.suggest_freq(('微博'), True) 强调特殊名词 不进行切分
 cut_text = ' '.join(jieba.cut(f))
 word_cloud = WordCloud(
     # 设置字体，不然会出现口字乱码，文字的路径是电脑的字体一般路径，可以换成别的
